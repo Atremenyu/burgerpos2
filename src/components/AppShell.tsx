86ca4ecf0b36d3bfddf38bbaf493f1ea0ec885a9
Refugio Oyervides <atreart@gmail.com>
2025-07-03 16:36:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -92,11 +92,3 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
     </div>
   );
 }
-
-// Minimal ThemeProvider to be used in layout
-import { ThemeProvider as NextThemesProvider } from 'next-themes'
-import type { ThemeProviderProps } from 'next-themes/dist/types'
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
